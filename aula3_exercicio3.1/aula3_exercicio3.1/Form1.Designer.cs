dec68eb220c8f5b5c43955c055efc720c570b730
TIAGO FIGUEIRA <tiago.figueira01@fatec.sp.gov.br>
2022-10-22 16:47:36 -0300

    novos_arquivos

---
+++ aula3_exercicio3.1/aula3_exercicio3.1/Form1.Designer.cs
@@ -0,0 +1,291 @@
+﻿
+namespace aula3_exercicio3._1
+{
+    partial class calc1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Lblladoquadrado = new System.Windows.Forms.Label();
+            this.resultadoquadrado = new System.Windows.Forms.Label();
+            this.btncalcularquadrado = new System.Windows.Forms.Button();
+            this.txtResultadoQuadrado = new System.Windows.Forms.TextBox();
+            this.txtLado = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Lblladoquadrado
+            // 
+            this.Lblladoquadrado.AutoSize = true;
+            this.Lblladoquadrado.Location = new System.Drawing.Point(7, 19);
+            this.Lblladoquadrado.Name = "Lblladoquadrado";
+            this.Lblladoquadrado.Size = new System.Drawing.Size(33, 15);
+            this.Lblladoquadrado.TabIndex = 1;
+            this.Lblladoquadrado.Text = "Lado";
+            this.Lblladoquadrado.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // resultadoquadrado
+            // 
+            this.resultadoquadrado.AutoSize = true;
+            this.resultadoquadrado.Location = new System.Drawing.Point(84, 56);
+            this.resultadoquadrado.Name = "resultadoquadrado";
+            this.resultadoquadrado.Size = new System.Drawing.Size(59, 15);
+            this.resultadoquadrado.TabIndex = 4;
+            this.resultadoquadrado.Text = "Resultado";
+            // 
+            // btncalcularquadrado
+            // 
+            this.btncalcularquadrado.Location = new System.Drawing.Point(0, 52);
+            this.btncalcularquadrado.Name = "btncalcularquadrado";
+            this.btncalcularquadrado.Size = new System.Drawing.Size(75, 23);
+            this.btncalcularquadrado.TabIndex = 5;
+            this.btncalcularquadrado.Text = "Calcular";
+            this.btncalcularquadrado.UseVisualStyleBackColor = true;
+            this.btncalcularquadrado.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtResultadoQuadrado
+            // 
+            this.txtResultadoQuadrado.Location = new System.Drawing.Point(149, 52);
+            this.txtResultadoQuadrado.Name = "txtResultadoQuadrado";
+            this.txtResultadoQuadrado.ReadOnly = true;
+            this.txtResultadoQuadrado.Size = new System.Drawing.Size(168, 23);
+            this.txtResultadoQuadrado.TabIndex = 6;
+            // 
+            // txtLado
+            // 
+            this.txtLado.Location = new System.Drawing.Point(149, 11);
+            this.txtLado.Name = "txtLado";
+            this.txtLado.Size = new System.Drawing.Size(168, 23);
+            this.txtLado.TabIndex = 7;
+            this.txtLado.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.Lblladoquadrado);
+            this.groupBox1.Controls.Add(this.txtResultadoQuadrado);
+            this.groupBox1.Controls.Add(this.txtLado);
+            this.groupBox1.Controls.Add(this.resultadoquadrado);
+            this.groupBox1.Controls.Add(this.btncalcularquadrado);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(337, 99);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Área do Quadrado";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.textBox2);
+            this.groupBox2.Controls.Add(this.textBox1);
+            this.groupBox2.Controls.Add(this.button1);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Location = new System.Drawing.Point(18, 129);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(330, 121);
+            this.groupBox2.TabIndex = 9;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Área do Retângulo";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.textBox5);
+            this.groupBox3.Controls.Add(this.textBox3);
+            this.groupBox3.Controls.Add(this.textBox4);
+            this.groupBox3.Controls.Add(this.label5);
+            this.groupBox3.Controls.Add(this.button2);
+            this.groupBox3.Controls.Add(this.label4);
+            this.groupBox3.Controls.Add(this.label3);
+            this.groupBox3.Location = new System.Drawing.Point(19, 280);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(330, 159);
+            this.groupBox3.TabIndex = 10;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Área do Triângulo";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 15);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Lado";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1, 72);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Calcular";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(143, 22);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(168, 23);
+            this.textBox1.TabIndex = 8;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(143, 72);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(168, 23);
+            this.textBox2.TabIndex = 8;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(82, 75);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 15);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Resultado";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 32);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(33, 15);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Lado";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 66);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(33, 15);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Lado";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(5, 102);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Calcular";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(86, 106);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(59, 15);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Resultado";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(142, 66);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(168, 23);
+            this.textBox3.TabIndex = 10;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(142, 29);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(168, 23);
+            this.textBox4.TabIndex = 11;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(142, 106);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.ReadOnly = true;
+            this.textBox5.Size = new System.Drawing.Size(168, 23);
+            this.textBox5.TabIndex = 10;
+            // 
+            // calc1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(394, 451);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "calc1";
+            this.Text = "Cálculo de áreas";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label Lblladoquadrado;
+        private System.Windows.Forms.Label resultadoquadrado;
+        private System.Windows.Forms.Button btncalcularquadrado;
+        private System.Windows.Forms.TextBox txtResultadoQuadrado;
+        private System.Windows.Forms.TextBox txtLado;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox5;
+    }
+}
+
---
+++ mediasapp/mediasapp/Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿
+namespace mediasapp
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtnota1 = new System.Windows.Forms.TextBox();
+            this.txtnota4 = new System.Windows.Forms.TextBox();
+            this.txtnota3 = new System.Windows.Forms.TextBox();
+            this.txtnota2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtresultado = new System.Windows.Forms.TextBox();
+            this.btnmedia_aritmetica = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnmedia_geometrica = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nota1";
+            // 
+            // txtnota1
+            // 
+            this.txtnota1.Location = new System.Drawing.Point(125, 21);
+            this.txtnota1.Name = "txtnota1";
+            this.txtnota1.Size = new System.Drawing.Size(180, 23);
+            this.txtnota1.TabIndex = 1;
+            // 
+            // txtnota4
+            // 
+            this.txtnota4.Location = new System.Drawing.Point(125, 150);
+            this.txtnota4.Name = "txtnota4";
+            this.txtnota4.Size = new System.Drawing.Size(180, 23);
+            this.txtnota4.TabIndex = 2;
+            // 
+            // txtnota3
+            // 
+            this.txtnota3.Location = new System.Drawing.Point(125, 106);
+            this.txtnota3.Name = "txtnota3";
+            this.txtnota3.Size = new System.Drawing.Size(180, 23);
+            this.txtnota3.TabIndex = 3;
+            // 
+            // txtnota2
+            // 
+            this.txtnota2.Location = new System.Drawing.Point(125, 64);
+            this.txtnota2.Name = "txtnota2";
+            this.txtnota2.Size = new System.Drawing.Size(180, 23);
+            this.txtnota2.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(27, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 15);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Nota2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(27, 114);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 15);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Nota3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(27, 158);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(39, 15);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Nota4";
+            // 
+            // txtresultado
+            // 
+            this.txtresultado.Font = new System.Drawing.Font("Segoe UI", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtresultado.Location = new System.Drawing.Point(6, 98);
+            this.txtresultado.Name = "txtresultado";
+            this.txtresultado.ReadOnly = true;
+            this.txtresultado.Size = new System.Drawing.Size(287, 61);
+            this.txtresultado.TabIndex = 10;
+            // 
+            // btnmedia_aritmetica
+            // 
+            this.btnmedia_aritmetica.Location = new System.Drawing.Point(6, 35);
+            this.btnmedia_aritmetica.Name = "btnmedia_aritmetica";
+            this.btnmedia_aritmetica.Size = new System.Drawing.Size(121, 44);
+            this.btnmedia_aritmetica.TabIndex = 11;
+            this.btnmedia_aritmetica.Text = "Média Aritmética";
+            this.btnmedia_aritmetica.UseVisualStyleBackColor = true;
+            this.btnmedia_aritmetica.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnmedia_geometrica);
+            this.groupBox1.Controls.Add(this.btnmedia_aritmetica);
+            this.groupBox1.Controls.Add(this.txtresultado);
+            this.groupBox1.Location = new System.Drawing.Point(12, 192);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(316, 165);
+            this.groupBox1.TabIndex = 13;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Informe o que quer:";
+            // 
+            // btnmedia_geometrica
+            // 
+            this.btnmedia_geometrica.Location = new System.Drawing.Point(172, 35);
+            this.btnmedia_geometrica.Name = "btnmedia_geometrica";
+            this.btnmedia_geometrica.Size = new System.Drawing.Size(121, 44);
+            this.btnmedia_geometrica.TabIndex = 12;
+            this.btnmedia_geometrica.Text = "Média Geométrica";
+            this.btnmedia_geometrica.UseVisualStyleBackColor = true;
+            this.btnmedia_geometrica.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(344, 382);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtnota2);
+            this.Controls.Add(this.txtnota3);
+            this.Controls.Add(this.txtnota4);
+            this.Controls.Add(this.txtnota1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Cálculo de Médias";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtnota1;
+        private System.Windows.Forms.TextBox txtnota4;
+        private System.Windows.Forms.TextBox txtnota3;
+        private System.Windows.Forms.TextBox txtnota2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtresultado;
+        private System.Windows.Forms.Button btnmedia_aritmetica;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnmedia_geometrica;
+    }
+}
+
