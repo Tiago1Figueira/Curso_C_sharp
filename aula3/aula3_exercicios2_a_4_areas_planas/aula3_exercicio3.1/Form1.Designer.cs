42935c51b826f85c1e70dd8962c3287fc312e191
TIAGO FIGUEIRA <tiago.figueira01@fatec.sp.gov.br>
2022-10-26 21:35:04 -0300

    Atualização7 aula3

---
+++ aula3/aula3_exercicios2_a_4_areas_planas/aula3_exercicio3.1/Form1.Designer.cs
@@ -0,0 +1,311 @@
+﻿
+namespace aula3_exercicio3._1
+{
+    partial class calc1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtLado = new System.Windows.Forms.Label();
+            this.resultadoQuadrado = new System.Windows.Forms.Label();
+            this.btnCalculaAreaQuadrado = new System.Windows.Forms.Button();
+            this.txtResultadoQuadrado = new System.Windows.Forms.TextBox();
+            this.txtLadoQuadrado = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.txtAlturaRetangulo = new System.Windows.Forms.TextBox();
+            this.txtAlturaRetangulo1 = new System.Windows.Forms.Label();
+            this.resultadoRetangulo = new System.Windows.Forms.Label();
+            this.txtResultadoRetangulo = new System.Windows.Forms.TextBox();
+            this.txtBaseRetangulo = new System.Windows.Forms.TextBox();
+            this.btnCalculaAreaRetangulo = new System.Windows.Forms.Button();
+            this.txtBaseRetangulo1 = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.txtResultadoTriangulo = new System.Windows.Forms.TextBox();
+            this.txtAlturaTriangulo = new System.Windows.Forms.TextBox();
+            this.txtBaseTriangulo = new System.Windows.Forms.TextBox();
+            this.resultadoTriangulo = new System.Windows.Forms.Label();
+            this.btnCalculaAreaTriangulo = new System.Windows.Forms.Button();
+            this.txtAlturaTriangulo1 = new System.Windows.Forms.Label();
+            this.txtBaseTriangulo1 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtLado
+            // 
+            this.txtLado.AutoSize = true;
+            this.txtLado.Location = new System.Drawing.Point(9, 19);
+            this.txtLado.Name = "txtLado";
+            this.txtLado.Size = new System.Drawing.Size(33, 15);
+            this.txtLado.TabIndex = 1;
+            this.txtLado.Text = "Lado";
+            // 
+            // resultadoQuadrado
+            // 
+            this.resultadoQuadrado.AutoSize = true;
+            this.resultadoQuadrado.Location = new System.Drawing.Point(84, 56);
+            this.resultadoQuadrado.Name = "resultadoQuadrado";
+            this.resultadoQuadrado.Size = new System.Drawing.Size(59, 15);
+            this.resultadoQuadrado.TabIndex = 4;
+            this.resultadoQuadrado.Text = "Resultado";
+            // 
+            // btnCalculaAreaQuadrado
+            // 
+            this.btnCalculaAreaQuadrado.Location = new System.Drawing.Point(9, 51);
+            this.btnCalculaAreaQuadrado.Name = "btnCalculaAreaQuadrado";
+            this.btnCalculaAreaQuadrado.Size = new System.Drawing.Size(69, 23);
+            this.btnCalculaAreaQuadrado.TabIndex = 5;
+            this.btnCalculaAreaQuadrado.Text = "Calcular";
+            this.btnCalculaAreaQuadrado.UseVisualStyleBackColor = true;
+            this.btnCalculaAreaQuadrado.Click += new System.EventHandler(this.btnCalculaAreaQuadrado_Click);
+            // 
+            // txtResultadoQuadrado
+            // 
+            this.txtResultadoQuadrado.Location = new System.Drawing.Point(153, 52);
+            this.txtResultadoQuadrado.Name = "txtResultadoQuadrado";
+            this.txtResultadoQuadrado.ReadOnly = true;
+            this.txtResultadoQuadrado.Size = new System.Drawing.Size(168, 23);
+            this.txtResultadoQuadrado.TabIndex = 6;
+            // 
+            // txtLadoQuadrado
+            // 
+            this.txtLadoQuadrado.Location = new System.Drawing.Point(153, 11);
+            this.txtLadoQuadrado.Name = "txtLadoQuadrado";
+            this.txtLadoQuadrado.Size = new System.Drawing.Size(168, 23);
+            this.txtLadoQuadrado.TabIndex = 7;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtLado);
+            this.groupBox1.Controls.Add(this.txtResultadoQuadrado);
+            this.groupBox1.Controls.Add(this.txtLadoQuadrado);
+            this.groupBox1.Controls.Add(this.resultadoQuadrado);
+            this.groupBox1.Controls.Add(this.btnCalculaAreaQuadrado);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(337, 99);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Área do Quadrado";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.txtAlturaRetangulo);
+            this.groupBox2.Controls.Add(this.txtAlturaRetangulo1);
+            this.groupBox2.Controls.Add(this.resultadoRetangulo);
+            this.groupBox2.Controls.Add(this.txtResultadoRetangulo);
+            this.groupBox2.Controls.Add(this.txtBaseRetangulo);
+            this.groupBox2.Controls.Add(this.btnCalculaAreaRetangulo);
+            this.groupBox2.Controls.Add(this.txtBaseRetangulo1);
+            this.groupBox2.Location = new System.Drawing.Point(12, 129);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(337, 145);
+            this.groupBox2.TabIndex = 9;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Área do Retângulo";
+            // 
+            // txtAlturaRetangulo
+            // 
+            this.txtAlturaRetangulo.Location = new System.Drawing.Point(147, 61);
+            this.txtAlturaRetangulo.Name = "txtAlturaRetangulo";
+            this.txtAlturaRetangulo.Size = new System.Drawing.Size(168, 23);
+            this.txtAlturaRetangulo.TabIndex = 12;
+            // 
+            // txtAlturaRetangulo1
+            // 
+            this.txtAlturaRetangulo1.AutoSize = true;
+            this.txtAlturaRetangulo1.Location = new System.Drawing.Point(9, 69);
+            this.txtAlturaRetangulo1.Name = "txtAlturaRetangulo1";
+            this.txtAlturaRetangulo1.Size = new System.Drawing.Size(42, 15);
+            this.txtAlturaRetangulo1.TabIndex = 13;
+            this.txtAlturaRetangulo1.Text = "Altura ";
+            // 
+            // resultadoRetangulo
+            // 
+            this.resultadoRetangulo.AutoSize = true;
+            this.resultadoRetangulo.Location = new System.Drawing.Point(82, 109);
+            this.resultadoRetangulo.Name = "resultadoRetangulo";
+            this.resultadoRetangulo.Size = new System.Drawing.Size(59, 15);
+            this.resultadoRetangulo.TabIndex = 9;
+            this.resultadoRetangulo.Text = "Resultado";
+            // 
+            // txtResultadoRetangulo
+            // 
+            this.txtResultadoRetangulo.Location = new System.Drawing.Point(147, 100);
+            this.txtResultadoRetangulo.Name = "txtResultadoRetangulo";
+            this.txtResultadoRetangulo.ReadOnly = true;
+            this.txtResultadoRetangulo.Size = new System.Drawing.Size(168, 23);
+            this.txtResultadoRetangulo.TabIndex = 8;
+            // 
+            // txtBaseRetangulo
+            // 
+            this.txtBaseRetangulo.Location = new System.Drawing.Point(147, 22);
+            this.txtBaseRetangulo.Name = "txtBaseRetangulo";
+            this.txtBaseRetangulo.Size = new System.Drawing.Size(168, 23);
+            this.txtBaseRetangulo.TabIndex = 8;
+            // 
+            // btnCalculaAreaRetangulo
+            // 
+            this.btnCalculaAreaRetangulo.Location = new System.Drawing.Point(4, 105);
+            this.btnCalculaAreaRetangulo.Name = "btnCalculaAreaRetangulo";
+            this.btnCalculaAreaRetangulo.Size = new System.Drawing.Size(68, 23);
+            this.btnCalculaAreaRetangulo.TabIndex = 8;
+            this.btnCalculaAreaRetangulo.Text = "Calcular";
+            this.btnCalculaAreaRetangulo.UseVisualStyleBackColor = true;
+            this.btnCalculaAreaRetangulo.Click += new System.EventHandler(this.btnCalculaAreaRetangulo_Click);
+            // 
+            // txtBaseRetangulo1
+            // 
+            this.txtBaseRetangulo1.AutoSize = true;
+            this.txtBaseRetangulo1.Location = new System.Drawing.Point(9, 30);
+            this.txtBaseRetangulo1.Name = "txtBaseRetangulo1";
+            this.txtBaseRetangulo1.Size = new System.Drawing.Size(34, 15);
+            this.txtBaseRetangulo1.TabIndex = 8;
+            this.txtBaseRetangulo1.Text = "Base ";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.txtResultadoTriangulo);
+            this.groupBox3.Controls.Add(this.txtAlturaTriangulo);
+            this.groupBox3.Controls.Add(this.txtBaseTriangulo);
+            this.groupBox3.Controls.Add(this.resultadoTriangulo);
+            this.groupBox3.Controls.Add(this.btnCalculaAreaTriangulo);
+            this.groupBox3.Controls.Add(this.txtAlturaTriangulo1);
+            this.groupBox3.Controls.Add(this.txtBaseTriangulo1);
+            this.groupBox3.Location = new System.Drawing.Point(12, 296);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(340, 159);
+            this.groupBox3.TabIndex = 10;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Área do Triângulo";
+            // 
+            // txtResultadoTriangulo
+            // 
+            this.txtResultadoTriangulo.Location = new System.Drawing.Point(146, 105);
+            this.txtResultadoTriangulo.Name = "txtResultadoTriangulo";
+            this.txtResultadoTriangulo.ReadOnly = true;
+            this.txtResultadoTriangulo.Size = new System.Drawing.Size(168, 23);
+            this.txtResultadoTriangulo.TabIndex = 10;
+            // 
+            // txtAlturaTriangulo
+            // 
+            this.txtAlturaTriangulo.Location = new System.Drawing.Point(146, 67);
+            this.txtAlturaTriangulo.Name = "txtAlturaTriangulo";
+            this.txtAlturaTriangulo.Size = new System.Drawing.Size(168, 23);
+            this.txtAlturaTriangulo.TabIndex = 10;
+            // 
+            // txtBaseTriangulo
+            // 
+            this.txtBaseTriangulo.Location = new System.Drawing.Point(146, 29);
+            this.txtBaseTriangulo.Name = "txtBaseTriangulo";
+            this.txtBaseTriangulo.Size = new System.Drawing.Size(168, 23);
+            this.txtBaseTriangulo.TabIndex = 11;
+            // 
+            // resultadoTriangulo
+            // 
+            this.resultadoTriangulo.AutoSize = true;
+            this.resultadoTriangulo.Location = new System.Drawing.Point(81, 114);
+            this.resultadoTriangulo.Name = "resultadoTriangulo";
+            this.resultadoTriangulo.Size = new System.Drawing.Size(59, 15);
+            this.resultadoTriangulo.TabIndex = 10;
+            this.resultadoTriangulo.Text = "Resultado";
+            // 
+            // btnCalculaAreaTriangulo
+            // 
+            this.btnCalculaAreaTriangulo.Location = new System.Drawing.Point(2, 106);
+            this.btnCalculaAreaTriangulo.Name = "btnCalculaAreaTriangulo";
+            this.btnCalculaAreaTriangulo.Size = new System.Drawing.Size(68, 23);
+            this.btnCalculaAreaTriangulo.TabIndex = 10;
+            this.btnCalculaAreaTriangulo.Text = "Calcular";
+            this.btnCalculaAreaTriangulo.UseVisualStyleBackColor = true;
+            this.btnCalculaAreaTriangulo.Click += new System.EventHandler(this.btnCalculaAreaTriangulo_Click);
+            // 
+            // txtAlturaTriangulo1
+            // 
+            this.txtAlturaTriangulo1.AutoSize = true;
+            this.txtAlturaTriangulo1.Location = new System.Drawing.Point(6, 66);
+            this.txtAlturaTriangulo1.Name = "txtAlturaTriangulo1";
+            this.txtAlturaTriangulo1.Size = new System.Drawing.Size(42, 15);
+            this.txtAlturaTriangulo1.TabIndex = 11;
+            this.txtAlturaTriangulo1.Text = "Altura ";
+            // 
+            // txtBaseTriangulo1
+            // 
+            this.txtBaseTriangulo1.AutoSize = true;
+            this.txtBaseTriangulo1.Location = new System.Drawing.Point(6, 32);
+            this.txtBaseTriangulo1.Name = "txtBaseTriangulo1";
+            this.txtBaseTriangulo1.Size = new System.Drawing.Size(31, 15);
+            this.txtBaseTriangulo1.TabIndex = 10;
+            this.txtBaseTriangulo1.Text = "Base";
+            // 
+            // calc1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(359, 507);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "calc1";
+            this.Text = "Cálculo de áreas";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label txtLado;
+        private System.Windows.Forms.Label resultadoQuadrado;
+        private System.Windows.Forms.Button btnCalculaAreaQuadrado;
+        private System.Windows.Forms.TextBox txtResultadoQuadrado;
+        private System.Windows.Forms.TextBox txtLadoQuadrado;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox txtAlturaTriangulo;
+        private System.Windows.Forms.Label resultadoRetangulo;
+        private System.Windows.Forms.TextBox txtResultadoRetangulo;
+        private System.Windows.Forms.TextBox txtBaseRetangulo;
+        private System.Windows.Forms.Button btnCalculaAreaRetangulo;
+        private System.Windows.Forms.Label txtBaseRetangulo1;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.TextBox txtBaseTriangulo;
+        private System.Windows.Forms.Label resultadoTriangulo;
+        private System.Windows.Forms.Button btnCalculaAreaTriangulo;
+        private System.Windows.Forms.Label txtAlturaTriangulo1;
+        private System.Windows.Forms.Label txtBaseTriangulo1;
+        private System.Windows.Forms.TextBox txtResultadoTriangulo;
+        private System.Windows.Forms.TextBox txtAlturaRetangulo;
+        private System.Windows.Forms.Label txtAlturaRetangulo1;
+    }
+}
+
---
+++ aula3/aula3_exercicios2_a_4_areas_planas/aula3_exercicio3.1/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace aula3_exercicio3._1
+{
+    public partial class calc1 : Form
+    {
+        public calc1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculaAreaRetangulo_Click(object sender, EventArgs e)
+        {
+            double baseRetangulo = Convert.ToDouble(txtBaseRetangulo.Text);
+            double alturaRetangulo = Convert.ToDouble(txtAlturaRetangulo.Text);
+            double area = baseRetangulo * alturaRetangulo;
+            txtResultadoRetangulo.Text = area.ToString();
+
+
+        }
+
+        private void btnCalculaAreaQuadrado_Click(object sender, EventArgs e)
+        {
+            double lado = Convert.ToDouble(txtLadoQuadrado.Text);
+            double area = lado * lado;
+            txtResultadoQuadrado.Text = area.ToString();
+        }
+
+        private void btnCalculaAreaTriangulo_Click(object sender, EventArgs e)
+        {
+            double baseTriangulo = Convert.ToDouble(txtBaseTriangulo.Text);
+            double altura = Convert.ToDouble(txtAlturaTriangulo.Text);
+            double area = (baseTriangulo * altura) / 2;
+            txtResultadoTriangulo.Text = area.ToString();
+
+        }
+    }
+}
