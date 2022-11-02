89f6a53119952d747985037b33860091666f0438
TIAGO FIGUEIRA <tiago.figueira01@fatec.sp.gov.br>
2022-11-02 20:09:57 -0300

    Atualização aula4.9

--- aula4/aula4_exercicio9/aula4_exercicio9/Form1.Designer.cs
+++ aula4/aula4_exercicio9/aula4_exercicio9/Form1.Designer.cs
@@ -29,13 +29,172 @@ namespace aula4_exercicio9
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtNumero1 = new System.Windows.Forms.TextBox();
+            this.txtNumero3 = new System.Windows.Forms.TextBox();
+            this.txtNumero2 = new System.Windows.Forms.TextBox();
+            this.btnProcessar = new System.Windows.Forms.Button();
+            this.txtResultado1 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtResultado3 = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 38);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Número1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(7, 94);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Número2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(7, 150);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(74, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Número3";
+            // 
+            // txtNumero1
+            // 
+            this.txtNumero1.Location = new System.Drawing.Point(98, 32);
+            this.txtNumero1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtNumero1.Name = "txtNumero1";
+            this.txtNumero1.Size = new System.Drawing.Size(221, 26);
+            this.txtNumero1.TabIndex = 3;
+            // 
+            // txtNumero3
+            // 
+            this.txtNumero3.Location = new System.Drawing.Point(98, 144);
+            this.txtNumero3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtNumero3.Name = "txtNumero3";
+            this.txtNumero3.Size = new System.Drawing.Size(221, 26);
+            this.txtNumero3.TabIndex = 4;
+            // 
+            // txtNumero2
+            // 
+            this.txtNumero2.Location = new System.Drawing.Point(98, 88);
+            this.txtNumero2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtNumero2.Name = "txtNumero2";
+            this.txtNumero2.Size = new System.Drawing.Size(221, 26);
+            this.txtNumero2.TabIndex = 5;
+            // 
+            // btnProcessar
+            // 
+            this.btnProcessar.Location = new System.Drawing.Point(5, 193);
+            this.btnProcessar.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnProcessar.Name = "btnProcessar";
+            this.btnProcessar.Size = new System.Drawing.Size(314, 35);
+            this.btnProcessar.TabIndex = 6;
+            this.btnProcessar.Text = "Processar";
+            this.btnProcessar.UseVisualStyleBackColor = true;
+            this.btnProcessar.Click += new System.EventHandler(this.btnProcessar_Click);
+            // 
+            // txtResultado1
+            // 
+            this.txtResultado1.Location = new System.Drawing.Point(98, 251);
+            this.txtResultado1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtResultado1.Name = "txtResultado1";
+            this.txtResultado1.Size = new System.Drawing.Size(221, 26);
+            this.txtResultado1.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(-1, 258);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(91, 20);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Resultado1";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.txtResultado3);
+            this.groupBox1.Controls.Add(this.txtNumero1);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.txtResultado1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.btnProcessar);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.txtNumero2);
+            this.groupBox1.Controls.Add(this.txtNumero3);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(328, 333);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Insira 3 números:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(-1, 301);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(91, 20);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Resultado3";
+            // 
+            // txtResultado3
+            // 
+            this.txtResultado3.Location = new System.Drawing.Point(98, 294);
+            this.txtResultado3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtResultado3.Name = "txtResultado3";
+            this.txtResultado3.Size = new System.Drawing.Size(221, 26);
+            this.txtResultado3.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(350, 359);
+            this.Controls.Add(this.groupBox1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Identifier";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtNumero1;
+        private System.Windows.Forms.TextBox txtNumero3;
+        private System.Windows.Forms.TextBox txtNumero2;
+        private System.Windows.Forms.Button btnProcessar;
+        private System.Windows.Forms.TextBox txtResultado1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtResultado3;
     }
 }
 
--- aula4/aula4_exercicio9/aula4_exercicio9/Form1.cs
+++ aula4/aula4_exercicio9/aula4_exercicio9/Form1.cs
@@ -16,5 +16,41 @@ namespace aula4_exercicio9
         {
             InitializeComponent();
         }
+
+        private void btnProcessar_Click(object sender, EventArgs e)
+        {
+            double numero1 = Convert.ToDouble(txtNumero1.Text);
+            double numero2 = Convert.ToDouble(txtNumero2.Text);
+            double numero3 = Convert.ToDouble(txtNumero3.Text);
+
+            double resultado; 
+            
+
+            if (numero1 > 0)
+            {
+                resultado = numero1 * 2;
+                txtResultado1.Text = resultado.ToString();
+            }else
+            {
+                resultado = numero1 * 3;
+                txtResultado1.Text = resultado.ToString();
+            }
+
+            if (numero2 > 10 && numero2 < 100)
+            
+            {
+                MessageBox.Show("Intervalo Permitido!");
+            }
+            if (numero3 < numero2)
+            {
+                resultado = numero3 - numero2;
+                txtResultado3.Text = resultado.ToString();
+            }
+            else
+            {
+                resultado = numero3 + 1;
+                txtResultado3.Text = resultado.ToString();
+            }
+        }
     }
 }
