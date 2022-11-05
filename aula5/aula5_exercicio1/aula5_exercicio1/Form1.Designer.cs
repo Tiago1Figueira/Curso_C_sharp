031439800c31568332bd8ca958cff5c5c4729699
TIAGO FIGUEIRA <tiago.figueira01@fatec.sp.gov.br>
2022-11-05 15:19:12 -0300

    Atualização aula5.1

---
+++ aula5/aula5_exercicio1/aula5_exercicio1/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿
+namespace aula5_exercicio1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtAnoNascimento = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtResultado = new System.Windows.Forms.TextBox();
+            this.btnPossoVotar = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(17, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(146, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ano de Nascimento";
+            // 
+            // txtAnoNascimento
+            // 
+            this.txtAnoNascimento.Location = new System.Drawing.Point(173, 37);
+            this.txtAnoNascimento.Name = "txtAnoNascimento";
+            this.txtAnoNascimento.Size = new System.Drawing.Size(113, 29);
+            this.txtAnoNascimento.TabIndex = 1;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txtResultado);
+            this.groupBox1.Controls.Add(this.btnPossoVotar);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.txtAnoNascimento);
+            this.groupBox1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(302, 180);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Insira o seu ano nascimento:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(17, 138);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(93, 21);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Sua idade é:";
+            // 
+            // txtResultado
+            // 
+            this.txtResultado.Location = new System.Drawing.Point(138, 130);
+            this.txtResultado.Name = "txtResultado";
+            this.txtResultado.Size = new System.Drawing.Size(148, 29);
+            this.txtResultado.TabIndex = 3;
+            // 
+            // btnPossoVotar
+            // 
+            this.btnPossoVotar.Location = new System.Drawing.Point(17, 83);
+            this.btnPossoVotar.Name = "btnPossoVotar";
+            this.btnPossoVotar.Size = new System.Drawing.Size(269, 36);
+            this.btnPossoVotar.TabIndex = 2;
+            this.btnPossoVotar.Text = "Posso votar?";
+            this.btnPossoVotar.UseVisualStyleBackColor = true;
+            this.btnPossoVotar.Click += new System.EventHandler(this.btnPossoVotar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(325, 201);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Identificador para votação";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtAnoNascimento;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtResultado;
+        private System.Windows.Forms.Button btnPossoVotar;
+    }
+}
+
---
+++ aula5/aula5_exercicio1/aula5_exercicio1/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace aula5_exercicio1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPossoVotar_Click(object sender, EventArgs e)
+        {
+            int ano = Convert.ToInt32(txtAnoNascimento.Text);
+
+            int resultado;
+
+            resultado = 2022 - ano;
+
+            if (resultado >= 16)
+            {
+                MessageBox.Show("Pode votar!");
+                txtResultado.Text = resultado.ToString();
+            }else
+            {
+                MessageBox.Show("Não poderá votar!!");
+                txtResultado.Text = resultado.ToString();
+            }
+        }
+    }
+}
