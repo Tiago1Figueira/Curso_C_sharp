1ef36cb9ae0407779d1a9decfdff945af085a7cf
TIAGO FIGUEIRA <tiago.figueira01@fatec.sp.gov.br>
2022-11-28 21:56:07 -0300

    atualizações aula9

---
+++ aula9/aula9_ex8/aula9_ex8/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace aula9_ex8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe um valor inteiro e positivo:");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int i;
+            double fatorial = 1, e = 0;
+            for (i = 1; i <= n; i++)
+            {
+                fatorial = fatorial * i;
+                e = e + (1 / fatorial);
+            }                       
+                Console.WriteLine("Fatorial:" + fatorial);
+                Console.WriteLine("E = " + e);
+            
+            Console.ReadKey();
+
+
+        }
+    }
+}
